b855475558b72c8454a7723df04bda9a6766224d
Krithik chandravel A <tysoneditz76@gmail.com>
2026-09-25 14:30:58 +0530

    Time: 10 ms (99.73%), Space: 43.8 MB (90.01%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.java
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.java
@@ -1,68 +1,71 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
 class Solution {
-    static Map<Integer,Integer> map;
-    static int BFS(TreeNode root){
-        int max = 0;
+
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+
+        List<Integer> result = new ArrayList<>();
+
+        find(root, target, k, result);
+
+        return result;
+    }
+
+    int find(TreeNode root, TreeNode target, int k, List<Integer> result) {
+
         if(root==null){
+            return -1;
+        }
+
+        if(root==target){
+            collect(root,k,result);
             return 0;
         }
-        Queue<TreeNode> q = new LinkedList<>();
-        q.offer(root);
-        while(!q.isEmpty()){
-            TreeNode temp = q.poll();
-            
-            if(temp.left!=null){
-                max = Math.max(max,temp.left.val);
-                map.put(temp.left.val,temp.val);
-                q.offer(temp.left);
-            }
-            if(temp.right!=null){
-                max = Math.max(max,temp.right.val);
-                map.put(temp.right.val,temp.val);
-                q.offer(temp.right);
-            }
-        }
-        return max;
-    }
-    static void Dfs(List<List<Integer>> adj,int node,boolean[] seen,int count,int k,List<Integer> curr){
-        seen[node] = true;
-        if(count==k){
-            curr.add(node);
-        }
-        for(int nei : adj.get(node)){
-            if(!seen[nei]){
-                Dfs(adj,nei,seen,count+1,k,curr);
-            }
-        }
-    }
-    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
-        if(root==null) return new ArrayList<>();
-        map = new HashMap<>();
-        int max = BFS(root);
-        if(max<=1) return new ArrayList<>();
-        List<List<Integer>> adj = new ArrayList<>();
-        for(int i=0;i<=max;i++){
-            adj.add(new ArrayList<>());
-        }
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-             int u = entry.getKey();
-             int v = entry.getValue();
 
-            adj.get(u).add(v);
-            adj.get(v).add(u);
-        }
-        boolean[] seen = new boolean[max+1];
-        List<Integer> result = new ArrayList<>();
-        Dfs(adj,target.val,seen,0,k,result);
-        return result;
+        int left = find(root.left,target,k,result);
 
+        if(left!=-1){
+            int distance = left+1;
+
+            if(distance==k){
+                result.add(root.val);
+                
+            }
+            else{
+                collect(root.right,k-distance-1,result);
+            }
+
+            return distance;
+        }
+
+        int right = find(root.right,target,k,result);
+
+        if(right!=-1){
+            int distance = right+1;
+
+            if(distance==k){
+                result.add(root.val);
+                
+            }
+            else{
+                collect(root.left,k-distance-1,result);
+            }
+            return distance;
+        }
+
+        return -1;
+    }
+
+    void collect(TreeNode root, int k, List<Integer> result) {
+
+        if (root == null || k < 0) {
+            return;
+        }
+
+        if (k == 0) {
+            result.add(root.val);
+            return;
+        }
+
+        collect(root.left, k - 1, result);
+        collect(root.right, k - 1, result);
     }
 }
